e223ff68e8bdbf69ab3fef860b4bca20b038f26b
Alan Smith <commit@alanwsmith.com>
2023-07-30 13:35:22 -0400

    - receiving from iterm2

--- src/main.rs
+++ src/main.rs
@@ -50,10 +50,10 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(index))
-        .route("/websocket", get(websocket_handler))
+        .route("/ws", get(websocket_handler))
         .with_state(app_state);
 
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 5757));
     tracing::debug!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
@@ -65,6 +65,7 @@ async fn websocket_handler(
     ws: WebSocketUpgrade,
     State(state): State<Arc<AppState>>,
 ) -> impl IntoResponse {
+    dbg!("hit");
     ws.on_upgrade(|socket| websocket(socket, state))
 }
 
@@ -75,34 +76,35 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     // By splitting, we can send and receive at the same time.
     let (mut sender, mut receiver) = stream.split();
 
-    // Username gets set in the receive loop, if it's valid.
-    let mut username = String::new();
-    // Loop until a text message is found.
+    // // Username gets set in the receive loop, if it's valid.
+    // let mut username = String::new();
+    // // Loop until a text message is found.
     while let Some(Ok(message)) = receiver.next().await {
         if let Message::Text(name) = message {
-            // If username that is sent by client is not taken, fill username string.
-            check_username(&state, &mut username, &name);
-
-            // If not empty we want to quit the loop else we want to quit function.
-            if !username.is_empty() {
-                break;
-            } else {
-                // Only send our client that username is taken.
-                let _ = sender
-                    .send(Message::Text(String::from("Username already taken.")))
-                    .await;
-
-                return;
-            }
+            //         // If username that is sent by client is not taken, fill username string.
+            //         check_username(&state, &mut username, &name);
+            //         // If not empty we want to quit the loop else we want to quit function.
+            //         if !username.is_empty() {
+            //             break;
+            //         } else {
+            //             // Only send our client that username is taken.
+            //             let _ = sender
+            //                 .send(Message::Text(String::from("Username already taken.")))
+            //                 .await;
+            //             return;
+            //         }
+            dbg!(name);
         }
     }
 
     // We subscribe *before* sending the "joined" message, so that we will also
     // display it to our client.
+
     let mut rx = state.tx.subscribe();
 
     // Now send the "joined" message to all subscribers.
-    let msg = format!("{} joined.", username);
+    // let msg = format!("{} joined.", username);
+    let msg = format!("joined.");
     tracing::debug!("{}", msg);
     let _ = state.tx.send(msg);
 
@@ -119,14 +121,15 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
 
     // Clone things we want to pass (move) to the receiving task.
     let tx = state.tx.clone();
-    let name = username.clone();
+    // let name = username.clone();
 
     // Spawn a task that takes messages from the websocket, prepends the user
     // name, and sends them to all broadcast subscribers.
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(Message::Text(text))) = receiver.next().await {
             // Add username before message.
-            let _ = tx.send(format!("{}: {}", name, text));
+            // let _ = tx.send(format!("{}: {}", name, text));
+            let _ = tx.send(format!("{}", text));
         }
     });
 
@@ -137,26 +140,25 @@ async fn websocket(stream: WebSocket, state: Arc<AppState>) {
     };
 
     // Send "user left" message (similar to "joined" above).
-    let msg = format!("{} left.", username);
+    // let msg = format!("{} left.", username);
+    let msg = format!("left.");
     tracing::debug!("{}", msg);
     let _ = state.tx.send(msg);
 
     // Remove username from map so new clients can take it again.
-    state.user_set.lock().unwrap().remove(&username);
+    // state.user_set.lock().unwrap().remove(&username);
 }
 
 fn check_username(state: &AppState, string: &mut String, name: &str) {
     let mut user_set = state.user_set.lock().unwrap();
-
     if !user_set.contains(name) {
         user_set.insert(name.to_owned());
-
         string.push_str(name);
     }
 }
 
 // Include utf-8 file at **compile** time.
 async fn index() -> Html<&'static str> {
-    Html(std::include_str!("../chat.html"))
+    //Html(std::include_str!("../chat.html"))
+    Html(std::include_str!("home_page.html"))
 }
-
