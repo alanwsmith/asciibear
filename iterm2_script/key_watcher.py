a7f255b67c56e4d9a4bd96afdc7b8c821e3d90cc
Alan Smith <commit@alanwsmith.com>
2023-07-31 15:22:43 -0400

    - possibly working with better async that only connects once

--- iterm2_script/key_watcher.py
+++ iterm2_script/key_watcher.py
@@ -3,16 +3,24 @@ import iterm2
 import websockets
 
 async def main(connection):
+    queue = asyncio.Queue()
+    task_holder = []
+    a = asyncio.create_task(watchit(connection, queue))
+    b = asyncio.create_task(sendit(queue))
+
+async def watchit(connection, queue):
     async with iterm2.KeystrokeMonitor(connection) as mon:
         while True:
             thekey = await mon.async_get()
-            print(thekey)
-            await sendit()
+            pring("got key")
+            queue.put_nowait("ping")
 
-async def sendit():
+async def sendit(queue):
     async with websockets.connect("ws://localhost:5757/ws") as websocket:
-        await websocket.send('{ "type": "key", "value": "HIDDEN_FOR_SECURITY"}')
-        await websocket.close()
+        while True:
+            ping = await queue.get()
+            hitit = await websocket.send('{ "type": "key", "value": "HIDDEN_FOR_SECURITY"}')
+            pring("sent key")
 
-iterm2.run_until_complete(main, True)
+iterm2.run_forever(main, True)
 
