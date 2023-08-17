01f3dbf69256eab50ebc6d33cd6c4e853a6be528
Alan Smith <commit@alanwsmith.com>
2023-08-16 20:42:51 -0400

    - values coming in from opencv

--- src/screen_capture.rs
+++ src/screen_capture.rs
@@ -29,6 +29,7 @@ use opencv::viz::Color;
 use opencv::{highgui, videoio};
 use opencv::{prelude::*, Result};
 use tokio::time::Duration;
+use serde_json::json;
 
 pub async fn screen_capture(tx: tokio::sync::broadcast::Sender<String>) {
 
@@ -66,88 +67,106 @@ pub async fn screen_capture(tx: tokio::sync::broadcast::Sender<String>) {
     loop {
 
         
-        let _ = tx.send("{}".to_string());
-        tokio::time::sleep(Duration::from_secs(1)).await;
+
+
+
         
         // let payload = r#"{"key": "screen_position", "x": "x"}"#.to_string();
         // let _ = tx.send(payload);
 
-    //     if loaded == false {
-    //         //cam.read(&mut first)?;
-    //         cam.read(&mut first).unwrap();
-    //         loaded = true;
-    //     } else {
-    //         second = first.clone();
-    //         cam.read(&mut first).unwrap();
-    //         // cam.read(&mut first)?;
+        if loaded == false {
+            //cam.read(&mut first)?;
+            cam.read(&mut first).unwrap();
+            loaded = true;
+        } else {
+            second = first.clone();
+            cam.read(&mut first).unwrap();
+            // cam.read(&mut first)?;
 
-    //         let mut tmp: Mat = Default::default();
+            let mut tmp: Mat = Default::default();
 
-    //         let mut lower: Vector<i32> = Default::default();
-    //         let mut upper: Vector<i32> = Default::default();
-    //         lower.push(254);
-    //         lower.push(254);
-    //         lower.push(254);
-    //         upper.push(255);
-    //         upper.push(255);
-    //         upper.push(255);
+            let mut lower: Vector<i32> = Default::default();
+            let mut upper: Vector<i32> = Default::default();
+            lower.push(254);
+            lower.push(254);
+            lower.push(254);
+            upper.push(255);
+            upper.push(255);
+            upper.push(255);
 
-    //         let _ = in_range(&second, &lower, &upper, &mut tmp);
+            let _ = in_range(&second, &lower, &upper, &mut tmp);
 
-    //         let mut thresh = Mat::default();
+            let mut thresh = Mat::default();
 
-    //         let mut contours: VectorOfVectorOfPoint = Default::default();
+            let mut contours: VectorOfVectorOfPoint = Default::default();
 
-    //         let p: Point = Default::default();
-    //         let _ = find_contours(&tmp, &mut contours, RETR_LIST, CHAIN_APPROX_SIMPLE, p);
+            let p: Point = Default::default();
+            let _ = find_contours(&tmp, &mut contours, RETR_LIST, CHAIN_APPROX_SIMPLE, p);
 
-    //         let mut cont_counter = 0.0;
-    //         let mut x: f32 = 0.0;
-    //         let mut y: f32 = 0.0;
+            let mut cont_counter = 0.0;
+            let mut x: f32 = 0.0;
+            let mut y: f32 = 0.0;
 
-    //         contours.iter().for_each(|cont| {
-    //             cont.clone().into_iter().for_each(|c| {
-    //                 cont_counter += 1.0;
-    //                 x += c.x as f32;
-    //                 y += c.y as f32;
-    //             });
-    //         });
+            contours.iter().for_each(|cont| {
+                cont.clone().into_iter().for_each(|c| {
+                    cont_counter += 1.0;
+                    x += c.x as f32;
+                    y += c.y as f32;
+                });
+            });
 
-    //         x = x / cont_counter as f32;
-    //         y = y / cont_counter as f32;
+            x = x / cont_counter as f32;
+            y = y / cont_counter as f32;
 
-    //         // let mut payload = r#"{"key": "screen_position", "x": "#.to_string();
-    //         // let xi = x as u32;
-    //         // let yi = y as u32;
-    //         // let x2 = xi.to_string();
-    //         // let y2 = yi.to_string();
-    //         // payload.push_str(x2.as_str());
-    //         // payload.push_str(r#", "y": ""#);
-    //         // payload.push_str(y2.as_str());
-    //         // payload.push_str(r#"}"#);
+            
+            let xi = x as u32;
+            let yi = y as u32;
+            // let x2 = xi.to_string();
+            // let y2 = yi.to_string();
+            // let mut payload = r#"
+            //     {
+            //     "key": "screen_position", 
+            //     "x": ""#.to_string();
+            //     payload.push_str(x2.as_str());
+            // payload.push_str(r#", "y": ""#);
+            // payload.push_str(y2.as_str());
+            // payload.push_str(r#"}"#);
 
 
-    //         let payload = r#"{"key": "screen_position", "x": "x"}"#.to_string();
+            let payload = json!({
+                "key": "screen_position",
+                "x": xi,
+                "y": yi});
 
-    //         // let _ = tx.send(payload);
 
-    // //         dbg!(&x);
-    // //         dbg!(&y);
-    // //         let mut tcv: VectorOfPoint = Default::default();
-    // //         tcv.push(Point::new(x as i32, y as i32));
 
-    // //         let b: Rect = bounding_rect(&tcv).unwrap();
-    // //         let s: VecN<f64, 4> = Default::default();
-    // //         let _ = rectangle(&mut tmp, b, s, 30, 0, 0);
+           // let payload = r#"{"key": "screen_position", "x": "x"}"#.to_string();
 
-    // //         //highgui::imshow(window, &tmp)?;
+            let _ = tx.send(payload.to_string());
+            tokio::time::sleep(Duration::from_secs(1)).await;
 
-    // //         // let key = highgui::wait_key(10)?;
-    // //         // if key > 0 && key != 255 {
-    // //         //     break;
-    // //         // }
+
+            // let _ = tx.send(payload);
+
+    //         dbg!(&x);
+    //         dbg!(&y);
+    //         let mut tcv: VectorOfPoint = Default::default();
+    //         tcv.push(Point::new(x as i32, y as i32));
+
+    //         let b: Rect = bounding_rect(&tcv).unwrap();
+    //         let s: VecN<f64, 4> = Default::default();
+    //         let _ = rectangle(&mut tmp, b, s, 30, 0, 0);
+
+    //         //highgui::imshow(window, &tmp)?;
+
+    //         // let key = highgui::wait_key(10)?;
+    //         // if key > 0 && key != 255 {
+    //         //     break;
+    //         // }
        
-        // }
+
+
+        }
      }
 
      //std::thread::sleep(std::time::Duration::from_secs(32536000));
